ac6076233a09f0f11b50b802b62903bb4ffdd8e0
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-06-08 13:42:46 -0700

    learn python string

---
+++ 03_String/main.py
@@ -0,0 +1,65 @@
+print('jm-Developer') # single quote
+print('i\'m jm-kodigu!')
+print("hello, i'm jm-kodigu from Timor-Leste.")
+print('"jm": i love python-languague')
+print("\"jm\": i love python-languague")
+print('hello, i\'m "jm" from TL, i want to become python developer!')
+print("hello, i'm \"jm\" from TL, i want to become python developer!")
+print("First Line\nSecond Line")
+# print("C:\some\name")
+print(r"C:\some\name")
+print('\n')
+
+print("""\
+my complete name is : juliao martins
+i come from : Timor-Leste
+i am : 18 years old
+hobyy : coding</>
+""")
+
+# concetaned with + operator and * to repeated
+print("jul" + "iao")
+print("j" * 10)
+print("=" * 30)
+print('j' * 3 + 'm')
+
+print('ju' 'liao')
+print('love' ' ' 'python')
+
+info = ("i'm jm-kodigu and i want to become python developer, "
+	"must learn everyday!")
+
+print(info)
+
+print(30*'=')
+
+name = "juliao"
+
+print(name[0]) # character in position 0
+print(name[5]) # character in position 5
+print(name[-1])
+print(name[-3])
+print(name[-6])
+
+# slicing
+print(name[0:2])
+print(name[:2] + name[2:])
+print(name[:4] + name[4:])
+
+print(name[:3])
+print(name[3:])
+print(name[-2:])
+
+print(name[4:42])
+print(name[42:])
+
+print('x' + name[1:])
+print(name[:2] + 'ju')
+
+print(len("juliaodejesusmartinspythondeveloper!"))
+
+# string methods()
+print('hello, world!'.title())
+print('hello, world!'.upper())
+print('Hello, World!'.lower())
+print(' Hello, world! '.strip())
