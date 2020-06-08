18492e21388627e3bc0d46535ed92455e4270f33
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-06-08 16:01:23 -0700

    learn range() built in function

---
+++ 08_Range()/main.py
@@ -0,0 +1,31 @@
+for i in range(10):
+	print(i)
+
+print(10*"=")
+
+for i in range(2,8):
+	print(i)
+
+print(10*"=")
+
+for i in range(0,20,3):
+	print(i)
+
+print(10*"=")
+
+# negative
+for i in range(-20,-200,-85):
+	print(i)
+
+print(10*"=","\n")
+
+a = ["juliao", "martins", "is", "a", "teacher"]
+
+for i in range(len(a)):
+	print(i, a[i])
+
+print('\n')
+
+print(range(0,10))
+print(sum(range(3)))
+print(list(range(4)))
