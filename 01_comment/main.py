ff67cffefc07b3ea3dd3adb30c0e9ea6ea00e790
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-06-08 11:38:21 -0700

    learn python comment

---
+++ 01_comment/main.py
@@ -0,0 +1,7 @@
+# first comment, i write
+spam = 1 # this is a comment
+text = "# this is not comment, because it's inside quote."
+
+# my complete name is juliao martins
+name = "juliao" # i'm from Timor-Leste
+# i love python languague
