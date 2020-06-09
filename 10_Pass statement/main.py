1e0ce3d4f67d9873f502806f692e5cd395eef445
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-06-08 19:41:03 -0700

    learn pass statement

---
+++ 10_Pass statement/main.py
@@ -0,0 +1,28 @@
+for i in range(2,25,3):
+	if i == 17:
+		print("your numbers is", i)
+		print("Hello #1")
+		pass
+		print("Hello #2")
+	print(i)
+else:
+	print("outside loops!")
+
+"""
+while True:
+	pass
+"""
+
+# empty class
+class MyEmptyClass:
+	pass
+
+obj1 = MyEmptyClass()	
+
+print(MyEmptyClass)
+print(obj1)
+
+def initlog(*args):
+	pass
+
+initlog()
