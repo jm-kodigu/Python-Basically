9a08d9b33ec7dae939f829815a5ce0bc3afa4e29
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-06-09 11:53:37 -0700

    learn del statement

---
+++ 16_Del statement/main.py
@@ -0,0 +1,21 @@
+a = [12,2.100,21.9,200.1,999.0,843]
+
+del a[0] # delete items index position 0
+
+print(a)
+
+# can be slice
+del a[2:4]
+
+print(a)
+
+name = "jmkodigu"
+
+print(name)
+
+del name
+
+"""
+print(name)
+can access because deleted
+"""
