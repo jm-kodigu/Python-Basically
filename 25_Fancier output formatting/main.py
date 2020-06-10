4d1945a8e031825c86938cdfeb7d282ca9634ddc
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-06-10 08:39:36 -0700

    learn how to formatting string with fancier & format() method

---
+++ 25_Fancier output formatting/main.py
@@ -0,0 +1,12 @@
+location = "Dili"
+year = 2020
+
+print(f"{location} in {year}")
+
+table = {111:"Joaquim",112:"aurito",113:"basilio"}
+for reg,name in table.items():
+	print(f"n0 reg. : {reg}\n\tname : {name}")
+
+jm = "python"
+
+print(f"my favorite programming languague is {jm}")
---
+++ 26_String Format() method/main.py
@@ -0,0 +1,15 @@
+print("i am {}, from {}".format("jm","timor-leste"))
+
+animal = "tiger"
+fruit = "orange"
+
+# print("{0} and {1}".format(animal,fruit))
+# print("{1} and {0}".format(animal,fruit))
+
+print("{hello} jm from {country}".format(hello="i'm",country="timor-leste"))
+
+# mix
+print("The story of {0}, {1} and {other}".format("me","family",other="friends"))
+
+for i in range(1,11):
+	print("{0:2d}, {1:3d}, {2:4d}".format(i,i*i,i*i*i))
