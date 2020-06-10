0f62530d19d16dd030fd0a868d2f424fb3db7ddf
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-06-10 12:58:04 -0700

    learn class object #intro

---
+++ 30_Class Object/main.py
@@ -0,0 +1,21 @@
+# template
+class Estudent:
+	# class variable
+	# name = '"name"'
+	# id = 0
+	pass
+	
+
+# object
+alexa = Estudent()
+alexa.name = "alexa"
+alexa.id = 111
+
+angelina = Estudent()
+angelina.name = "angelina"
+angelina.id = 112
+
+print(alexa.name)
+print(alexa.id)
+print(angelina.name)
+print(angelina.id)
