32118f7a5e1d3b175f1e732fb013a82ecd1e5436
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-06-11 19:17:48 -0700

    learn abstract base class & decorator

---
+++ 49_Abstract Base Class/main.py
@@ -0,0 +1,34 @@
+from abc import ABC,abstractmethod
+
+# class abstract
+class Button(ABC):
+
+	# constructor __init__()
+	def __init__(self,seTlink):
+		self.link = seTlink
+
+	@abstractmethod
+	def click(self):
+		pass
+
+	@property
+	def link(self):
+		pass
+
+# class / instance
+class Pushbutton(Button):
+
+	def click(self):
+		print('go to : https://' + self.link)
+
+	@Button.link.setter
+	def link(self,input):
+		self.__link = input
+
+	@link.getter
+	def link(self):
+		return self.__link
+
+button1 = Pushbutton('www.jm-kodigu.com')
+
+button1.click()
